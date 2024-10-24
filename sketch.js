cebf1725529d4684f36f89e5289d14e2bd1835a2
dMario24 <data.mario24@gmail.com>
2024-10-24 11:06:57 +0900

    first deploy

--- sketch.js
+++ sketch.js
@@ -1,7 +1,25 @@
+function getWidth() {
+  return windowWidth - 10;
+}
+
+function getHeight() {
+  return windowHeight - 10;
+}
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(getWidth(), getHeight());
 }
 
 function draw() {
-  background(220);
+  background("aqua");
+
+  if(mouseIsPressed == true) {
+    fill(0);
+  } else {
+    fill(255);
+  }
+
+  circle(mouseX, getHeight() - 100 / 2, 100);
+  
+  arc(getWidth() / 2, mouseY, 80, 80, 0, PI + QUARTER_PI, PIE, 5);
 }
